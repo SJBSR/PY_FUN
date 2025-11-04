72fbaa116096b5f627152c4f9fde6dfadc1e81d5
SJBSR <sjb.sr@outlook.com>
2025-11-03 22:50:16 -0500

    Area (L*W) and Tax Calculator

---
+++ py_fun_02.py
@@ -0,0 +1,9 @@
+# Tax Calculator
+
+# Welcome message
+print ("Hello! Welcome to the Tax Calculator!")
+
+amount = int(1000)
+tax = 0.0625
+total = amount + (amount * tax)
+print("The total amount including tax is:", total)
