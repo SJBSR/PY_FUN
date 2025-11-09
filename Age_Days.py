e77652870f8af6b7f1bb3f37e8b1b3c5ca4d0210
SJBSR <sjb.sr@outlook.com>
2025-11-09 17:28:03 -0500

    Ticket_Generator (#5)
    * SB 11/9
    
    * Ticket Generator started

--- Age_Days.py
+++ Age_Days.py
@@ -1,6 +1,6 @@
 # This is an Age Calculator in days
 
-# Ask the user for their age in years
+# Ask the user for their age in years (\n creates a new line for the user's input)
 age_years = input("What is your age in years?\n")
 
 # Set variable to convert years to days
@@ -9,5 +9,5 @@ days_in_year = 365
 # Calculate the user's age in days
 age_days = int(age_years) * days_in_year
 
-# Print out the user's age in days
+# Print out the user's age in days (using an f-string)
 print(f"You're {age_days} days old!")
