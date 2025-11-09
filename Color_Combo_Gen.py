535b7484de6ab68d455e3fe81f9ad1de67f76c37
SJBSR <sjb.sr@outlook.com>
2025-11-09 16:32:22 -0500

    SB 11/9

---
+++ Color_Combo_Gen.py
@@ -0,0 +1,11 @@
+# Random color generator
+import random
+
+colors_list = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Brown', 'Black', 'White']
+
+
+# Random color generator to append to above printed color
+color_combo_list = ['Cyan', 'Magenta', 'Lime', 'Teal', 'Violet', 'Indigo', 'Gold', 'Silver', 'Maroon', 'Navy']
+
+random_color = random.choice(colors_list) + " & " + random.choice(color_combo_list)
+print(f"Your random color combination is: {random_color}")
--- py_fun_RES.py
+++
@@ -1,8 +0,0 @@
-# Random color generator
-import random
-
-def generate_random_color():
-    r = random.randint(0, 255)  # Red component
-    g = random.randint(0, 255)  # Green component
-    b = random.randint(0, 255)  # Blue component
-    return (r, g, b)
