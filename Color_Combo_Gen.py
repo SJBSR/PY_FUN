ba91e4dd9c707d0e98a1561251e747da915ebb08
SJBSR <sjb.sr@outlook.com>
2025-11-10 19:50:57 -0500

    11/10 SJB (#6)
    * 11/10 SB

--- Color_Combo_Gen.py
+++ Color_Combo_Gen.py
@@ -3,7 +3,6 @@ import random
 
 colors_list = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Brown', 'Black', 'White']
 
-
 # Random color generator to append to above printed color
 color_combo_list = ['Cyan', 'Magenta', 'Lime', 'Teal', 'Violet', 'Indigo', 'Gold', 'Silver', 'Maroon', 'Navy']
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PY_FUN
 py_fun
+
+This is where py makes everything fun
+
+This is a virtual notebook to take notes on python
